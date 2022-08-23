78dd4ee86045214e45e1b46d0f0240fc3fa9834c
Raphael Ribeiro Lino <raphael.lino@gpnet.com.br>
2022-08-23 16:05:44 -0300

    Atualizando api

--- src/main/java/br/com/aceleragep/biblioteca/configs/ControllerConfig.java
+++
@@ -1,6 +0,0 @@
-package br.com.aceleragep.biblioteca.configs;
-
-public class ControllerConfig {
-
-	public final static String PRE_URL = "/api";
-}
--- src/main/java/br/com/aceleragep/biblioteca/repositories/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.aceleragep.biblioteca.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.aceleragep.biblioteca.entities.AutorEntity;
-
-public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
-
-}
